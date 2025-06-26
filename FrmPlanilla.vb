b5abd8a6fd2c3fe31981b739c3bb6ade942b0a6e
Angel moncada <agpmoncada2006@gmail.com>
2025-06-25 20:20:41 -0600

    se modifico y mejoro el total del dtg de la planilla

--- FrmPlanilla.vb
+++ FrmPlanilla.vb
@@ -34,6 +34,8 @@ Public Class FrmPlanilla
         Next
         MskCedula.Clear()
         MskCedula.Focus()
+        ActualizarFilaTotales()
+
 
     End Sub
 
@@ -43,15 +45,140 @@ Public Class FrmPlanilla
         Else
             MsgBox("Seleccione una fila para eliminar.")
         End If
+        ActualizarFilaTotales()
+    End Sub
 
+    Private Sub ActualizarFilaTotales()
+        ' Eliminar fila TOTAL anterior
+        For Each fila As DataGridViewRow In DtgPlanilla.Rows
+            If Not fila.IsNewRow AndAlso fila.Cells(0).Value?.ToString() = "TOTALES" Then
+                DtgPlanilla.Rows.Remove(fila)
+                Exit For
+            End If
+        Next
+
+        ' Crear nueva fila de totales
+        Dim filaTotal As New DataGridViewRow()
+        filaTotal.CreateCells(DtgPlanilla)
+
+        filaTotal.Cells(0).Value = "TOTALES"
+        filaTotal.Cells(0).Style.Font = New Font(DtgPlanilla.Font, FontStyle.Bold)
+
+        Dim totalSalMen As Decimal = 0
+        Dim totalSalDia As Decimal = 0
+        Dim totalHorasExtra As Decimal = 0
+
+        Dim totalSalarioBasico As Decimal = 0
+        Dim totalSubsidios As Decimal = 0
+        Dim totalFeriados As Decimal = 0
+
+        Dim totalVacaciones As Decimal = 0
+        Dim totalReajustes As Decimal = 0
+        Dim totalDevengados As Decimal = 0
+
+        Dim totalINNDSLAB As Decimal = 0
+        Dim totalPrestamos As Decimal = 0
+        Dim totalIR As Decimal = 0
+
+        Dim totalTdeducciones As Decimal = 0
+        Dim totalTotalAPagar As Decimal = 0
+        Dim totalINNSPatronal As Decimal = 0
+
+        Dim totalINATEC As Decimal = 0
+        Dim totalVacacionesPatronal As Decimal = 0
+        Dim totalAguinaldo As Decimal = 0
+
+        For Each fila As DataGridViewRow In DtgPlanilla.Rows
+            If Not fila.IsNewRow Then
+                If IsNumeric(fila.Cells(3).Value) Then
+                    totalSalMen += Convert.ToDecimal(fila.Cells(3).Value)
+                End If
+                If IsNumeric(fila.Cells(4).Value) Then
+                    totalSalDia += Convert.ToDecimal(fila.Cells(4).Value)
+                End If
+                If IsNumeric(fila.Cells(6).Value) Then
+                    totalHorasExtra += Convert.ToDecimal(fila.Cells(6).Value)
+                End If
+                If IsNumeric(fila.Cells(7).Value) Then
+                    totalSalarioBasico += Convert.ToDecimal(fila.Cells(7).Value)
+                End If
+                If IsNumeric(fila.Cells(8).Value) Then
+                    totalSubsidios += Convert.ToDecimal(fila.Cells(8).Value)
+                End If
+                If IsNumeric(fila.Cells(9).Value) Then
+                    totalFeriados += Convert.ToDecimal(fila.Cells(9).Value)
+                End If
+                If IsNumeric(fila.Cells(10).Value) Then
+                    totalVacaciones += Convert.ToDecimal(fila.Cells(10).Value)
+                End If
+                If IsNumeric(fila.Cells(11).Value) Then
+                    totalReajustes += Convert.ToDecimal(fila.Cells(11).Value)
+                End If
+                If IsNumeric(fila.Cells(12).Value) Then
+                    totalDevengados += Convert.ToDecimal(fila.Cells(12).Value)
+                End If
+                If IsNumeric(fila.Cells(13).Value) Then
+                    totalINNDSLAB += Convert.ToDecimal(fila.Cells(13).Value)
+                End If
+                If IsNumeric(fila.Cells(14).Value) Then
+                    totalPrestamos += Convert.ToDecimal(fila.Cells(14).Value)
+                End If
+                If IsNumeric(fila.Cells(15).Value) Then
+                    totalIR += Convert.ToDecimal(fila.Cells(15).Value)
+                End If
+                If IsNumeric(fila.Cells(16).Value) Then
+                    totalTdeducciones += Convert.ToDecimal(fila.Cells(16).Value)
+                End If
+                If IsNumeric(fila.Cells(17).Value) Then
+                    totalTotalAPagar += Convert.ToDecimal(fila.Cells(17).Value)
+                End If
+                If IsNumeric(fila.Cells(19).Value) Then
+                    totalINNSPatronal += Convert.ToDecimal(fila.Cells(18).Value)
+                End If
+                If IsNumeric(fila.Cells(20).Value) Then
+                    totalINATEC += Convert.ToDecimal(fila.Cells(19).Value)
+                End If
+                If IsNumeric(fila.Cells(21).Value) Then
+                    totalVacacionesPatronal += Convert.ToDecimal(fila.Cells(20).Value)
+                End If
+                If IsNumeric(fila.Cells(22).Value) Then
+                    totalAguinaldo += Convert.ToDecimal(fila.Cells(21).Value)
+                End If
+
+            End If
+        Next
+
+        filaTotal.Cells(3).Value = "C$" & totalSalMen.ToString("N2")
+        filaTotal.Cells(4).Value = "C$" & totalSalDia.ToString("N2")
+        filaTotal.Cells(6).Value = "C$" & totalHorasExtra.ToString("N2")
+        filaTotal.Cells(7).Value = "C$" & totalSalarioBasico.ToString("N2")
+        filaTotal.Cells(8).Value = "C$" & totalSubsidios.ToString("N2")
+        filaTotal.Cells(9).Value = "C$" & totalFeriados.ToString("N2")
+        filaTotal.Cells(10).Value = "C$" & totalVacaciones.ToString("N2")
+
+        filaTotal.Cells(12).Value = "C$" & totalDevengados.ToString("N2")
+        filaTotal.Cells(13).Value = "C$" & totalINNDSLAB.ToString("N2")
+        filaTotal.Cells(14).Value = "C$" & totalPrestamos.ToString("N2")
+        filaTotal.Cells(15).Value = "C$" & totalIR.ToString("N2")
+        filaTotal.Cells(16).Value = "C$" & totalTdeducciones.ToString("N2")
+        filaTotal.Cells(17).Value = "C$" & totalTotalAPagar.ToString("N2")
+        filaTotal.Cells(19).Value = "C$" & totalINNSPatronal.ToString("N2")
+        filaTotal.Cells(20).Value = "C$" & totalINATEC.ToString("N2")
+        filaTotal.Cells(21).Value = "C$" & totalVacacionesPatronal.ToString("N2")
+        filaTotal.Cells(22).Value = "C$" & totalAguinaldo.ToString("N2")
+        filaTotal.DefaultCellStyle.BackColor = Color.Beige
+        filaTotal.ReadOnly = True
+
+        DtgPlanilla.Rows.Add(filaTotal)
     End Sub
 
     Private Sub FrmPlanilla_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         DtgPlanilla.AllowUserToAddRows = False
+        ActualizarFilaTotales()
     End Sub
+
+
     'estas son las validaciones de los tekiste
-
-
 
     'CEDULA
     Private Sub TxtNombres_KeyUp(sender As Object, e As KeyEventArgs) Handles TxtNombres.KeyUp
@@ -220,9 +347,13 @@ Public Class FrmPlanilla
         End If
     End Sub
 
+
+
+    'CEDULA
     Private Sub MskCedula_Validating(sender As Object, e As CancelEventArgs) Handles MskCedula.Validating
-        If String.IsNullOrWhiteSpace(MskCedula.Text.Replace(" ", "").Replace("-", "")) Then
-            MsgBox("Por favor, no deje la cédula vacía.")
+        Dim cedulaSinFormato As String = MskCedula.Text.Replace(" ", "").Replace("-", "")
+        If String.IsNullOrWhiteSpace(cedulaSinFormato) OrElse cedulaSinFormato.Length < MskCedula.Mask.Replace("-", "").Length Then
+            MsgBox("Por favor, complete la cédula correctamente.")
             MskCedula.Clear()
             MskCedula.Focus()
             e.Cancel = True
